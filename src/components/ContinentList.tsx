4cf0e0ff1c56be4642c237e9cc2fde5a6ad108f3
Jedrzej Borakiewicz <jedrzej.borakiewicz@webcrafter.pl>
2021-06-30 19:32:55 +0200

    Install react-spinners-css, Create LoadingWrapper component, add styles with styled-components

--- src/components/ContinentList.tsx
+++ src/components/ContinentList.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 import { ContinentData } from '../common/interfaces/continents.interface';
-import {ButtonLink} from './ButtonLink';
+import { LoadingWrapper } from './LoadingWrapper';
+import { Grid } from 'react-spinners-css';
+import { ButtonLink } from './ButtonLink';
+import styled from 'styled-components';
+
+const ContinentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  margin: 0 auto;
+  overflow: hidden;
+  background-color: #191F2A;
+`
 
 const GET_CONTINENTS = gql`
   query Continents {
@@ -16,15 +30,15 @@ const GET_CONTINENTS = gql`
 const ContinentList: React.FC = () => {
   const { data, loading} = useQuery<ContinentData>(GET_CONTINENTS);
   if (loading) {
-    return <div>Loading...</div>;
+    return <LoadingWrapper><Grid/></LoadingWrapper>;
   }
 
   return (
-    <div>
+    <ContinentWrapper>
       {data?.continents.map((continent, i) => (
           <ButtonLink key={i} to={`/continents/${continent.code}`}>{continent.name}</ButtonLink>
       ))}
-    </div>
+    </ContinentWrapper>
   );
 };
 export default ContinentList;
--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -2,7 +2,62 @@ import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 import {useParams} from 'react-router-dom';
 import {IdTypes, CountryData} from '../common/interfaces/country.interface';
+import {LoadingWrapper} from './LoadingWrapper';
+import { Grid } from 'react-spinners-css';
+import styled from 'styled-components';
 
+const CountryBackground = styled.div`
+  min-height: 100vh;
+  margin: 0 auto;
+  overflow: hidden;
+  background-color: #191F2A;
+`
+
+const CountryWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 30% 30% 30%;
+  grid-gap: 10px;
+  justify-content: center;
+`
+
+const CountryBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 170px;
+  padding-top: 10px;
+  background-color: #222A3C;
+  color: #fff;
+  font-size: 1.2em;
+`
+
+const TopContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Emoji = styled.div`
+  font-size: 42px;
+`
+
+const BottomContent = styled.div`
+  text-align: center;
+`
+
+const Names = styled.p`
+  font-weight: bold;
+  margin-left: 10px;
+  font-size: 20px;
+`
+
+const LanguagesTitle = styled.p`
+  font-size: 16px;
+`
+
+const Languages = styled.p`
+  margin: 5px 0;
+`
 
 const GET_COUNTRIES = gql`
   query Countries($code: String) {
@@ -29,28 +84,28 @@ const CountryList: React.FC = () => {
   });
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <LoadingWrapper><Grid/></LoadingWrapper>;
   }
 
   return (
-    <div>
-      <div>
+    <CountryBackground>
+      <CountryWrapper>
         {data?.countries.map((country, i) => {
           return (
-          <div key={i}>
-            <div><div>{country.emoji}</div><div><p>{country.name}</p></div></div>
-            <div>
-              <p>Languages:</p>
+          <CountryBlock key={i}>
+            <TopContent><Emoji>{country.emoji}</Emoji><div><Names>{country.name}</Names></div></TopContent>
+            <BottomContent>
+              <LanguagesTitle>Languages:</LanguagesTitle>
             {country.languages.map((language, i) => {
               return (
-                <p key={i}>{i+1}-{language.name}</p>
+                <Languages key={i}>{i+1}-{language.name}</Languages>
               )
             })}
-            </div>
-          </div>
+            </BottomContent>
+          </CountryBlock>
         )})}
-      </div>
-    </div>
+      </CountryWrapper>
+    </CountryBackground>
   );
 };
 
